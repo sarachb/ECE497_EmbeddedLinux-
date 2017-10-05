db0360a42d6f79696bb9f7e70bf4e1bf2a0bcb73
Demo User <debian@beaglebone.localdomain>
2017-10-05 14:50:43 +0000

    adding commits and cleaning up code:

--- hw03/hw03.py
+++ hw03/hw03.py
@@ -38,17 +38,17 @@ count = 8
 x=0x01
 y=0x00
 
+#smile face for start
 smile = [0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85,
 0x04, 0x85, 0x28, 0x89, 0x00, 0x42, 0x00, 0x3c
 ]
 
+#game
 etch = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 ]
 
-clear = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-]
+
 
 
 # Start oscillator (p10)
@@ -58,21 +58,19 @@ bus.write_byte_data(matrix, 0x81, 0)
 # Full brightness (page 15)
 bus.write_byte_data(matrix, 0xe7, 0)
 
+#wirting smiley face to the matrix
 bus.write_i2c_block_data(matrix, 0, smile)
 
-# bus.write_byte_data(temp1, 3, 24) 
-# bus.write_byte_data(temp2, 3, 24) 
 
 map1 = {sensor: temp1, sensor2: temp2}
 start = True
-#clear = True
 
+#writing new matrix data
 def drawTable(grid):
     global matrix
-   
-    
     bus.write_i2c_block_data(matrix, 0, grid)
-    
+
+#updating new matrix values after event handler   
 def updateTable(channel):
     global count
     global x
@@ -80,62 +78,31 @@ def updateTable(channel):
     global start
     
     print ("BUTTON PRESSED")
-    print (etch)
-    print (x)
-    print(y)
-    
+
     if (((channel == "GP0_5") | (channel == "GP0_6") | (channel == "GP0_4") | (channel == "GP0_3")) & ( start == True)):
         drawTable(etch)
         start = False
         print ("Start by going right! Press Button 5")
-    elif (channel == "GP0_5") & ( x > 0x01):#(y < (count)): #right 
+    elif (channel == "GP0_5") & ( x > 0x01): #right 
         print("right")
         x = x >> 1
-        #if (y + 1)< (count): y = y+1
-    elif (channel == "GP0_3") & (y < 15):#( x > 1): #up
+    elif (channel == "GP0_3") & (y < 15): #up
         print("up")
         y = y +2
-        #if (x+1)> 0: x = x+1
-    elif (channel == "GP0_4") & (y > 0):#(x < (count)): #down
+    elif (channel == "GP0_4") & (y > 0): #down
         print("down")
         y = y -2
-        #if (x -1)< (count) : x= x-1
-    elif (channel == "GP0_6") & (x < 0x80):#(y > 1): #left
+    elif (channel == "GP0_6") & (x < 0x80): #left
         print("left")
         x = x << 1
-        #if (y-1) > 0: y=y-1
-    
         
-    # ref= (2*x) -1
-    # etch[ref] = etch[ref] | 0x01 << (count-y)
+    
     etch[y] = etch[y] | x
     
     drawTable(etch)
     
-    
-    
-        
-  
-   
-    
-# def alertDetect(channel):
-#     alertAddr = map1[channel]
-#     temp = bus.read_byte_data(alertAddr, 0)
-#     #print (temp)
-#     if (channel == "GP1_3") & (temp >= 27):
-#         print ("Etch")
-#         bus.write_i2c_block_data(matrix, 0, etch)
-#     if (channel == "GP1_4") & (temp >= 27):
-#         print ("Clear")
-#         bus.write_i2c_block_data(matrix, 0, clear)
-    
-    
-    
 
-
-# GPIO.add_event_detect(sensor, GPIO.BOTH, callback = alertDetect)
-#GPIO.add_event_detect(sensor2, GPIO.BOTH, callback = alertDetect)
-
+#event handler for each button
 GPIO.add_event_detect(buttonU, GPIO.BOTH, callback = updateTable)
 GPIO.add_event_detect(buttonD, GPIO.BOTH, callback = updateTable)
 GPIO.add_event_detect(buttonL, GPIO.BOTH, callback = updateTable)
