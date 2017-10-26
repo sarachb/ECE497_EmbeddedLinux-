f33b48578484eeb60cb75ae497df9890fad81245
Demo User <debian@beaglebone.localdomain>
2017-10-26 16:10:18 +0000

    adding work for hw5

---
+++ hw05/README.m
@@ -0,0 +1,7 @@
+The LEDs cycle through GREEN-YELLOW-RED-OFF.
+No matter what color the LED is set to, or if
+it is off it will continue that patter.
+
+The script must be run using sudo ./boneServer.js
+
+
